3342219195fe11a7795f72219ec10489a8be5967
SDFGSJ <nmps60601@gmail.com>
2022-05-29 00:34:36 +0800

    feat: AddTaskScreen add spaces

--- app/src/main/java/com/example/workordie/ui/screen/AddTaskScreen.kt
+++ app/src/main/java/com/example/workordie/ui/screen/AddTaskScreen.kt
@@ -3,59 +3,68 @@ package com.example.workordie.ui.screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.workordie.ui.theme.WorkOrDieTheme
 
 
 /* Not yet done:
-*  1. Top bar
-*  2. Add subtasks should be '+' icon
-*  3. Formatting
-*  4. cant work now,it's only ui
+*  Top bar
+*  Add subtasks should be '+' icon
+*  cant work now,it's only ui
 * */
 @Composable
 fun AddTaskBody(){
-    Column(){
+    Column(
+        modifier = Modifier.padding(30.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
         Text(
             text = "New Task",
-            textAlign = TextAlign.Center
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+            fontSize = 36.sp
         )
-        TextField(
+        OutlinedTextField(
             value = "機率",
             onValueChange = { },
             label = { Text(text = "Name") }
         )
-        TextField(
+        OutlinedTextField(
             value = "作業",
             onValueChange = { },
             label = { Text(text = "Type") }
         )
-        TextField(
+        OutlinedTextField(
             value = "5/30",
             onValueChange = { },
             label = { Text(text = "Deadline") }
         )
-        TextField(
+        OutlinedTextField(
             value = "5",
             onValueChange = { },
             label = { Text(text = "Estimated Time to Finish(in hour)") }
         )
-        TextField(
+        OutlinedTextField(
             value = "5/20",
             onValueChange = { },
             label = { Text(text = "Estimated Start Date") }
         )
-        TextField(
+        OutlinedTextField(
             value = "+",
             onValueChange = { },
             label = { Text(text = "Add subtasks") }
         )
         Button(
-            onClick = {  }
+            onClick = {  },
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
         ) {
             Text(text = "submit")
         }
