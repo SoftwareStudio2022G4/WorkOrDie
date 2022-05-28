ef7e361ba3e202a4f464c3595a78bcd0ef8dd672
SDFGSJ <nmps60601@gmail.com>
2022-05-28 23:15:59 +0800

    feat: AddTaskScreen finish 70%

--- app/src/main/java/com/example/workordie/ui/screen/AddTaskScreen.kt
+++ app/src/main/java/com/example/workordie/ui/screen/AddTaskScreen.kt
@@ -1,2 +1,73 @@
 package com.example.workordie.ui.screen
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.workordie.ui.theme.WorkOrDieTheme
+
+
+/* Not yet done:
+*  1. Top bar
+*  2. Add subtasks should be '+' icon
+*  3. Formatting
+*  4. cant work now,it's only ui
+* */
+@Composable
+fun AddTaskBody(){
+    Column(){
+        Text(
+            text = "New Task",
+            textAlign = TextAlign.Center
+        )
+        TextField(
+            value = "機率",
+            onValueChange = { },
+            label = { Text(text = "Name") }
+        )
+        TextField(
+            value = "作業",
+            onValueChange = { },
+            label = { Text(text = "Type") }
+        )
+        TextField(
+            value = "5/30",
+            onValueChange = { },
+            label = { Text(text = "Deadline") }
+        )
+        TextField(
+            value = "5",
+            onValueChange = { },
+            label = { Text(text = "Estimated Time to Finish(in hour)") }
+        )
+        TextField(
+            value = "5/20",
+            onValueChange = { },
+            label = { Text(text = "Estimated Start Date") }
+        )
+        TextField(
+            value = "+",
+            onValueChange = { },
+            label = { Text(text = "Add subtasks") }
+        )
+        Button(
+            onClick = {  }
+        ) {
+            Text(text = "submit")
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WorkOrDieTheme {
+        AddTaskBody()
+    }
+}
