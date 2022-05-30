36def8ce20e2513c0cbce0fb5df8c686111b9d60
SDFGSJ <nmps60601@gmail.com>
2022-05-30 22:57:57 +0800

    feat: HomeScreen complete 70%

--- app/src/main/java/com/example/workordie/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/workordie/ui/screen/HomeScreen.kt
@@ -1,2 +1,168 @@
 package com.example.workordie.ui.screen
 
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.workordie.ui.theme.WorkOrDieTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+
+/* reference link:
+https://www.jetpackcompose.net/scaffold
+https://www.youtube.com/watch?v=UDW4v88V41M
+* */
+
+/* TODOs
+cant find suitable icon for All Tasks
+the onclick of bottom bar item should be navigation in the future
+havent place the profile icon
+body content
+* */
+
+@Composable
+fun HomeBody(){
+    val scaffoldState : ScaffoldState = rememberScaffoldState(/*rememberDrawerState(DrawerValue.Closed)*/)
+    val scope : CoroutineScope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            MyTopBar(scaffoldState = scaffoldState, scope = scope)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}){
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "fab icon"
+                )
+            }
+        },
+        drawerContent = {
+            DrawerContent()
+        },
+        content = {
+            BodyContent()
+        },
+        bottomBar = {
+            MyBottomBar()
+        }
+    )
+}
+
+@Composable
+fun MyTopBar(scaffoldState : ScaffoldState, scope : CoroutineScope){
+    val drawerState = scaffoldState.drawerState
+
+    TopAppBar(
+        title = { },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    scope.launch {
+                        if(drawerState.isClosed){
+                            drawerState.open()
+                        }else{
+                            drawerState.close()
+                        }
+                    }
+                }
+            ) {
+                Icon(
+                    Icons.Default.Menu,
+                    contentDescription = "Menu"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun BodyContent(){
+    val date = Date()
+    val formatter = SimpleDateFormat("MMM dd")
+    val dateString: String = formatter.format(date)
+    Text(
+        text = dateString,
+        fontSize = 36.sp
+    )
+}
+
+@Composable
+fun DrawerContent(){
+    Text(text = "Drawer Menu 1")
+}
+
+@Composable
+fun MyBottomBar(){
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation(
+        elevation = 10.dp
+    ) {
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Edit ,
+                    contentDescription = "All Tasks"
+                )
+            },
+            label = {
+                Text(text = "All Tasks")
+            },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "Today's Task"
+                )
+            },
+            label = {
+                Text(text = "Today's Task")
+            },
+            selected = (selectedIndex.value == 1),
+            onClick = {
+                selectedIndex.value = 1
+            }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Calendar"
+                )
+            },
+            label = {
+                Text(text = "Calendar")
+            },
+            selected = (selectedIndex.value == 2),
+            onClick = {
+                selectedIndex.value = 2
+            }
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview() {
+    WorkOrDieTheme {
+        HomeBody()
+    }
+}
