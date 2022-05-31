93ba4f7eaec1eada491f4952f4c56aa52732c6bf
wewe5215 <76145143+wewe5215@users.noreply.github.com>
2022-05-31 16:38:06 +0800

    Revert "Dev countingtime"

--- app/src/main/java/com/example/workordie/ui/theme/Theme.kt
+++ app/src/main/java/com/example/workordie/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple400,
-    primaryVariant = Blue700,
-    secondary = Green400
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Grey900,
-    primaryVariant = Blue100,
-    secondary = Ocean100
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
