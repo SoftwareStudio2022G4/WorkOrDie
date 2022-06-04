ffd201c9935ec54d78fa74dfd1249b4a3fff5b32
Jenny <jennywei0102@gapp.nthu.edu.tw>
2022-06-04 21:52:20 +0800

    [1/2] update FinishSubmit

--- app/src/main/java/com/example/workordie/ui/screen/FinishSubmitScreen.kt
+++ app/src/main/java/com/example/workordie/ui/screen/FinishSubmitScreen.kt
@@ -1,68 +1,53 @@
 package com.example.workordie.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import com.example.workordie.ui.theme.WorkOrDieTheme
 import com.example.workordie.ui.theme.Yellow100
 
 /* TODO
-need a better format
+Background color
+Boxes of subtask 1 to 3
+onclick button
+Font, font size
 * */
 
 @Composable
-fun FinishSubmit(navController: NavController){
-    Column(
-        modifier = Modifier.padding(120.dp),
+fun FinishSubmitBody(){
+        Column(
+        modifier = Modifier.padding(30.dp),
         verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "You've added a new task",
-            fontSize = 28.sp
-        )
-
+        Text(text = "You've added a new task")
         Text(
             text = "<HW name>",
-            fontSize = 36.sp
+            fontSize = 25.sp
         )
-
         Text(
             text = "with subtasks below:"
         )
-
         for (i in 1..3){
-            Surface(
-                color = Yellow100
-            ) {
-                Text(
-                    text = "subtask $i"
-                )
-            }
+            Text(
+                text = "subtask $i"
+            )
         }
-
         //add some space between subtasks and button
         Spacer(modifier = Modifier.height(60.dp))
 
-        //after pressing Done button,we should pop all screens including Home screen in the backstack
-        //then we can navigate to Home screen
         Button(
-            onClick = {
-                navController.navigate(NavScreen.Home.route){
-                    popUpTo(NavScreen.Home.route){
-                        inclusive = true
-                    }
-                }
-            }
+            onClick = { /*TODO*/ }
         ) {
             Text(text = "Done")
             Icon(
@@ -73,10 +58,35 @@ fun FinishSubmit(navController: NavController){
     }
 }
 
-/*@Preview(showBackground = true)
+@Preview
+@Composable
+private fun Subtasks(/*...*/) {
+    Column() {
+        Row(
+            modifier = Modifier
+                .size(width = 150.dp, height = 30.dp)
+                .background(color = Yellow100)
+        ) {
+            Text(text = "Subtask 1")
+        }
+    }
+}
+
+@Composable
+private fun Subtask(name: String) {
+
+}
+
+@Composable
+private fun SubTaskContent(name: String) {
+
+}
+
+
+@Preview(showBackground = true)
 @Composable
 fun FinishSubmitPreview() {
     WorkOrDieTheme {
-        FinishSubmit()
+        FinishSubmitBody()
     }
-}*/
+}
