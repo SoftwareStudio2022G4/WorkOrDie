333191d4dd9da2f938e65c4104af787048d3aa4f
Jenny <jennywei0102@gapp.nthu.edu.tw>
2022-06-05 22:04:49 +0800

    Finish subtasks boxes but still need to change the UI

--- app/src/main/java/com/example/workordie/ui/screen/FinishSubmitScreen.kt
+++ app/src/main/java/com/example/workordie/ui/screen/FinishSubmitScreen.kt
@@ -1,20 +1,20 @@
 package com.example.workordie.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.workordie.ui.theme.WorkOrDieTheme
-import com.example.workordie.ui.theme.Yellow100
 
 /* TODO
 Background color
@@ -22,32 +22,44 @@ Boxes of subtask 1 to 3
 onclick button
 Font, font size
 * */
-
+//@PreviewParameter
 @Composable
-fun FinishSubmitBody(){
+fun FinishSubmit(navController: NavController){
         Column(
         modifier = Modifier.padding(30.dp),
         verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "You've added a new task")
+        Text(
+            text = "You've added a new task",
+            fontSize = 28.sp
+        )
         Text(
             text = "<HW name>",
-            fontSize = 25.sp
+            fontSize = 36.sp
         )
         Text(
             text = "with subtasks below:"
         )
-        for (i in 1..3){
+            WorkOrDieTheme{
+                Subtasks()
+            }
+        /*for (i in 1..3){
             Text(
                 text = "subtask $i"
             )
-        }
+        }*/
         //add some space between subtasks and button
         Spacer(modifier = Modifier.height(60.dp))
 
         Button(
-            onClick = { /*TODO*/ }
+            onClick = {
+                navController.navigate(NavScreen.Home.route){
+                    popUpTo(NavScreen.Home.route){
+                        inclusive = true
+                    }
+                }
+            }
         ) {
             Text(text = "Done")
             Icon(
@@ -60,33 +72,59 @@ fun FinishSubmitBody(){
 
 @Preview
 @Composable
-private fun Subtasks(/*...*/) {
-    Column() {
-        Row(
-            modifier = Modifier
-                .size(width = 150.dp, height = 30.dp)
-                .background(color = Yellow100)
-        ) {
-            Text(text = "Subtask 1")
+private fun Subtasks(names: List<String> = List(3) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 2.dp)) {
+        items(items = names) {name ->
+            Subtask(name = name)
         }
     }
 }
 
 @Composable
 private fun Subtask(name: String) {
-
+    // TODO: implement Card
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
 }
 
 @Composable
-private fun SubTaskContent(name: String) {
+private fun CardContent(name: String) {
 
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            // TODO: colour?
+    ) {
+        Text(text = "Subtask ")
+        Text(
+            text = name
+        )
+    }
 }
 
+/*
+@Preview
+@Composable
+fun TopAppBar(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colors.primarySurface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    elevation: Dp = AppBarDefaults.TopAppBarElevation,
+    contentPadding: PaddingValues = AppBarDefaults.ContentPadding,
+    content: (@Composable @ExtensionFunctionType RowScope.() -> Unit)?
+) {
+}
+*/
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun FinishSubmitPreview() {
     WorkOrDieTheme {
-        FinishSubmitBody()
+        FinishSubmit()
     }
-}
+}*/
