f38d95a4f2e35b4ca4e40d23697d5a64142e1e77
SDFGSJ <nmps60601@gmail.com>
2022-06-09 23:43:43 +0800

    fix: make code cleaner

--- app/src/main/java/com/example/workordie/ui/screen/CountingTime.kt
+++ app/src/main/java/com/example/workordie/ui/screen/CountingTime.kt
@@ -1,31 +1,21 @@
 package com.example.workordie.ui.screen
 
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.workordie.ui.CountDownTime.CountDownTimeViewModel
-import com.example.workordie.ui.CountDownTime.MyApp
-import com.example.workordie.ui.accumulateTime.MainApp
-import com.example.workordie.ui.accumulateTime.MainViewModel
 import com.example.workordie.ui.theme.WorkOrDieTheme
 import kotlinx.coroutines.CoroutineScope
-import kotlin.time.ExperimentalTime
 
 @Composable
 fun CountingTime(modifier: Modifier = Modifier, navController : NavController){
