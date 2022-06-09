38b0fcbb5ee5bbb1b5781e4ce3e767facf02261c
弘明 <73580589+SDFGSJ@users.noreply.github.com>
2022-06-09 21:30:48 +0800

    fix: comment out some code
    avoid name conflict

--- app/src/main/java/com/example/workordie/MainActivity.kt
+++ app/src/main/java/com/example/workordie/MainActivity.kt
@@ -5,17 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+//import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.workordie.ui.CountDownTime.MyApp
+/*import com.example.workordie.ui.CountDownTime.MyApp
 import com.example.workordie.ui.accumulateTime.MainApp
-import com.example.workordie.ui.accumulateTime.MainViewModel
+import com.example.workordie.ui.accumulateTime.MainViewModel*/
 import com.example.workordie.ui.screen.*
 import com.example.workordie.ui.theme.WorkOrDieTheme
-import kotlin.time.ExperimentalTime
+//import kotlin.time.ExperimentalTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +28,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalTime::class)
+//@OptIn(ExperimentalTime::class)
 @Composable
 fun WorkorDieApp(){
     //State Hoisting
     //use navController to navigate between screens
     val navController = rememberNavController()
-    val viewModel: MainViewModel = viewModel()
+    //val viewModel: MainViewModel = viewModel()
     //NavHost holds the NavGraph
     //list the screen you want to navigate below
     NavHost(
@@ -71,7 +71,7 @@ fun WorkorDieApp(){
         composable(route = NavScreen.Profile.route){
             Profile(navController = navController)
         }
-        composable(route = NavScreen.CountdownTime.route){
+        /*composable(route = NavScreen.CountdownTime.route){
             MyApp(navController = navController)
         }
         composable(route = NavScreen.AccumulateTime.route){
@@ -80,7 +80,7 @@ fun WorkorDieApp(){
                 viewModel = viewModel,
                 navController = navController
             )
-        }
+        }*/
     }
 }
 
