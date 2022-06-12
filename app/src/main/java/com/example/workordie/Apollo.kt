86329332d763e16d2a9bf647ff17463096c1e834
erichwu11 <erichwu11@gmail.com>
2022-06-12 18:34:06 +0800

    feat: try to execute query (success)

--- app/src/main/java/com/example/workordie/Apollo.kt
+++ app/src/main/java/com/example/workordie/Apollo.kt
@@ -3,5 +3,5 @@ package com.example.graphql
 import com.apollographql.apollo3.ApolloClient
 
 val apolloClient = ApolloClient.Builder()
-    .serverUrl("http://b477-140-114-221-77.ngrok.io/graphql")
+    .serverUrl("https://ssgroup4.herokuapp.com/graphql")
     .build()
